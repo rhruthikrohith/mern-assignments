34ca18d8f1a106f8d444576412cca953f464f473
rhruthikrohith <rhruthikrohith@gmail.com>
2026-02-07 18:40:04 +0530

    datehands.js

--- week2/datehands.js
+++ week2/datehands.js
@@ -1,6 +1,20 @@
-let date1=new Date()
-console.log(date1.getFullYear())
-console.log(date1.getMonth())
-console.log(date1.getDate())
-console.log(date1.getDay())
-console.log(date1.getHours(),date1.getMinutes(),date1.getSeconds())
+let date1 = new Date();
+
+// Current year
+console.log(date1.getFullYear());
+
+// Month (0 = Jan, 11 = Dec)
+console.log(date1.getMonth());
+
+// Day of the month
+console.log(date1.getDate());
+
+// Day of the week (0 = Sunday)
+console.log(date1.getDay());
+
+// Current time (hours, minutes, seconds)
+console.log(
+    date1.getHours(),
+    date1.getMinutes(),
+    date1.getSeconds()
+);
