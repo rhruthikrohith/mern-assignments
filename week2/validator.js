0723f20326ce202d1ce06222f2eb66371c1b2fe3
rhruthikrohith <rhruthikrohith@gmail.com>
2026-02-07 18:46:01 +0530

    validator.js

--- week2/validator.js
+++ week2/validator.js
@@ -1,12 +1,15 @@
+// Check if title is valid
 function validateTitle(title) {
   if (!title) return false;
   return title.length >= 3;
 }
 
+// Allow only valid priority values
 function validatePriority(priority) {
   return priority === "low" || priority === "medium" || priority === "high";
 }
 
+// Check if due date is in the future
 function validateDueDate(date) {
   const dueDate = new Date(date);
   const today = new Date();
@@ -14,3 +17,4 @@ function validateDueDate(date) {
 }
 
 export { validateTitle, validatePriority, validateDueDate };
+
