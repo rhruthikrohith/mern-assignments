3ef641640a9b5dc33817fcf2c8ea86cc990372c8
rhruthikrohith <rhruthikrohith@gmail.com>
2026-02-07 18:45:29 +0530

    task.js

--- week2/task.js
+++ week2/task.js
@@ -1,23 +1,33 @@
 import { validateTitle, validatePriority, validateDueDate } from "./validator.js";
+
 const tasks = [];
 let taskId = 1;
+
+// Add a new task
 function addTask(title, priority, dueDate) {
   if (!validateTitle(title)) return "Invalid title";
   if (!validatePriority(priority)) return "Invalid priority";
   if (!validateDueDate(dueDate)) return "Invalid due date";
+
   let task = {};
   task.id = taskId;
   task.title = title;
   task.priority = priority;
   task.dueDate = dueDate;
   task.completed = false;
+
   taskId = taskId + 1;
   tasks[tasks.length] = task;
+
   return "Task added successfully";
 }
+
+// Return all tasks
 function getAllTasks() {
   return tasks;
 }
+
+// Mark a task as completed
 function completeTask(taskId) {
   let task = tasks.find(t => t.id === taskId);
 
@@ -28,4 +38,5 @@ function completeTask(taskId) {
 
   return "Task not found";
 }
+
 export { addTask, getAllTasks, completeTask };
