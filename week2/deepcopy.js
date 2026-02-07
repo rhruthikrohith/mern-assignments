2396ea71b430c7e7fef90f6e8124b6063b5a06b5
rhruthikrohith <rhruthikrohith@gmail.com>
2026-02-07 18:40:32 +0530

    Update deepcopy.js

--- week2/deepcopy.js
+++ week2/deepcopy.js
@@ -1,18 +1,25 @@
+// Original order object
 const order = {
-                  orderId: "ORD1001",
-                  customer: {
-                    name: "Anita",
-                    address: {
-                      city: "Hyderabad",
-                      pincode: 500085
-                    }
-                  },
-                  items: [
-                    { product: "Laptop", price: 70000 }
-                  ]
-                };
-                let odd=structuredClone(order)
-                odd.customer.address.city="vijaywada"
-                odd.items[0].price=90;
-                console.log(order)
-                console.log(odd)
+    orderId: "ORD1001",
+    customer: {
+        name: "Anita",
+        address: {
+            city: "Hyderabad",
+            pincode: 500085
+        }
+    },
+    items: [
+        { product: "Laptop", price: 70000 }
+    ]
+};
+
+// Deep copy using structuredClone
+let odd = structuredClone(order);
+
+// Modify copied object
+odd.customer.address.city = "vijaywada";
+odd.items[0].price = 90;
+
+// Original object remains unchanged
+console.log(order);
+console.log(odd);
