fb2985dfcdc9432677bbda014ec6a79e4dbb7154
rhruthikrohith <rhruthikrohith@gmail.com>
2026-02-07 18:44:44 +0530

    shallowcopy.js

--- week2/shallowcopy.js
+++ week2/shallowcopy.js
@@ -1,15 +1,23 @@
- const user = {
-                id: 101,
-                name: "Ravi",
-                preferences: {
-                  theme: "dark",
-                  language: "en"
-                }
-              }
-              let copyobj={...user}
-              copyobj.name="rrr"
-              copyobj.preferences.theme="white"
-              user.log="jfn"
-              copyobj.log="djdj"
-              console.log(copyobj)
-              console.log(user)
+const user = {
+    id: 101,
+    name: "Ravi",
+    preferences: {
+        theme: "dark",
+        language: "en"
+    }
+};
+
+// Shallow copy using spread operator
+let copyobj = { ...user };
+
+// Modify copied object
+copyobj.name = "rrr";
+copyobj.preferences.theme = "white";
+
+// Add new properties
+user.log = "jfn";
+copyobj.log = "djdj";
+
+console.log(copyobj);
+console.log(user);
+
